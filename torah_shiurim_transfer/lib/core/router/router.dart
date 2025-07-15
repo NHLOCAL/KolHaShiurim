95ec40c56b2753c961e190529dfb890095761648
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-07-16 02:44:48 +0300

    תיקונים נוספים

--- torah_shiurim_transfer/lib/core/router/router.dart
+++ torah_shiurim_transfer/lib/core/router/router.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
-
-import 'package:flutter/foundation.dart'; // הוספת import זה
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:torah_shiurim_transfer/core/providers/providers.dart';
