abb8c0270024070592ced930c180a452fb59e08f
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-07-21 05:49:56 +0300

    Update run-repomix.bat

--- run-repomix.bat
+++ run-repomix.bat
@@ -1,5 +1,5 @@
 @echo off
 
-npx repomix "torah_shiurim_transfer\lib" --style markdown --remove-comments -i "assets/**,core/database/database.g.dart"
+npx repomix "torah_shiurim_transfer\lib" --style markdown --remove-comments -i "assets/**,core/database/database.g.dart,*.pem,keys/**"
 
 pause
