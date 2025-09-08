7ab5514c864d025f14a990eece8701b2d6c4b67c
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-09-09 01:31:47 +0300

    feat: Implement single-instance application lock

--- src/windows/runner/main.cpp
+++ src/windows/runner/main.cpp
@@ -7,6 +7,17 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
+  // Prevent multiple instances of the app from running.
+  HANDLE hMutex = CreateMutex(NULL, TRUE, L"KolHaShiurim.single.instance.mutex");
+  if (hMutex != NULL && GetLastError() == ERROR_ALREADY_EXISTS) {
+    HWND hWnd = FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"kol_hashiurim");
+    if (hWnd != NULL) {
+        ShowWindow(hWnd, SW_RESTORE);
+        SetForegroundWindow(hWnd);
+    }
+    return EXIT_FAILURE;
+  }
+
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -28,6 +39,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
   if (!window.Create(L"kol_hashiurim", origin, size)) {
+    if (hMutex != NULL) {
+      ReleaseMutex(hMutex);
+      CloseHandle(hMutex);
+    }
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
@@ -36,6 +51,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
+  }
+
+  if (hMutex != NULL) {
+    ReleaseMutex(hMutex);
+    CloseHandle(hMutex);
   }
 
   ::CoUninitialize();
